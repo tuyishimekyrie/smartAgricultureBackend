85a62e7948aa30f34054d364b9c745c71e17b55d
tuyishimekyrie <tuyishimehope01@gmail.com>
2024-11-20 11:51:27 +0200

    Initial Commit

---
+++ src/main/java/com/sensor/agri/smartagriculturebackend/SmartAgricultureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sensor.agri.smartagriculturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAgricultureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartAgricultureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sensor/agri/smartagriculturebackend/SmartAgricultureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sensor.agri.smartagriculturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAgricultureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
